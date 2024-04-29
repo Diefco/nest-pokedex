3c6493cc4aaa65409d31870f590c2f5ad260c7d9
Diego Díaz <diefco.web@gmail.com>
2024-04-28 20:14:06 -0500

    Add joi validation to the configuration.

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,13 @@ import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { ConfigModule } from '@nestjs/config';
 import { EnvConfiguration } from './config/app.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [EnvConfiguration], // opcional, para app.config.ts
+      load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema, // opcional, para app.config.ts
     }), // config para obtener .env. Es importante ponerlo lo más arriba posible.
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,5 +2,5 @@ export const EnvConfiguration = () => ({
   enviroment: process.env.NODE_ENV || 'dev',
   mongodb: process.env.MONGODB,
   port: process.env.PORT || 3000,
-  defaultLimit: process.env.DEFAULT_LIMIT || 7,
+  defaultLimit: +process.env.DEFAULT_LIMIT || 7, // Se convierte a numero para no recibirlo como string desde .env
 });
